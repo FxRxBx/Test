267695a8f5fbacd4dcfac11d896d6d411a9c1c6f
fxrxbx <105020032+fxrxbx@users.noreply.github.com>
2022-05-08 19:27:25 +0200

    Commit and push

--- standard_user Login.py
+++ standard_user Login.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome('/Users/fxrxbx/Downloads/chromedriver')
 
 driver.get("https://qa-challenge.codesubmit.io/")
+
+element = driver.find_element(By.ID, "user-name")
+element.clear()
+element.send_keys("standard_user")
