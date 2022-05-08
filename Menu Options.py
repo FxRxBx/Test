b9fbdbebc7918d2d34a2326bcda08ad4461c8a45
fxrxbx <105020032+fxrxbx@users.noreply.github.com>
2022-05-08 21:48:23 +0200

    Commit and push

---
+++ Menu Options.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome('/Users/fxrxbx/Downloads/chromedriver')
+
+driver.get("https://qa-challenge.codesubmit.io/")
+
+element = driver.find_element(By.ID, "user-name")
+element.clear()
+element.send_keys("standard_user")
+
+element = driver.find_element(By.ID, "password")
+element.clear()
+element.send_keys("secret_sauce")
+
+element = driver.find_element(By.CLASS_NAME, "react-burger-menu-btn")
+element.click()
+time.sleep(2)
+
+element = driver.find_element(By.CLASS_NAME, "inventory_sidebar_link")
+element.click()
+time.sleep(3)
+
+element = driver.find_element(By.CLASS_NAME, "about_sidebar_link")
+element.click()
+time.sleep(3)
+
+element = driver.find_element(By.CLASS_NAME, "reset_sidebar_link")
+element.click()
+time.sleep(3)
+
+element = driver.find_element(By.CLASS_NAME, "logout_sidebar_link")
+element.click()
