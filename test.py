75a319ee178dde7979f00784335b72aa45663dd6
fxrxbx <105020032+fxrxbx@users.noreply.github.com>
2022-05-10 14:13:27 +0200

    Commit and push

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
 
 driver = webdriver.Chrome('/Users/fxrxbx/Downloads/chromedriver')
 
@@ -7,7 +10,7 @@ driver.get("https://qa-challenge.codesubmit.io/")
 
 element = driver.find_element(By.ID, "user-name")
 element.clear()
-element.send_keys("locked_out_user")
+element.send_keys("problem_user")
 
 element = driver.find_element(By.ID, "password")
 element.clear()
@@ -16,3 +19,13 @@ element.send_keys("secret_sauce")
 element = driver.find_element(By.ID, "login-button")
 element.click()
 
+if(driver.findElement(By.id("reports").size()!=0){
+
+   WebElement menuHoverLink = driver.findElement(By.id("reports"));
+   actions.moveToElement(menuHoverLink).perform();
+
+   }
+   else{
+   system.out.println("element not present -- so it entered the else loop");
+
+}
